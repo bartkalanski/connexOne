2c2ab530966c55881ab478e61be3edefb7eb93ae
Bart Kalanski <bkalanski@gmail.com>
2023-07-28 14:31:28 +0100

    Create api

---
+++ API/src/index.ts
@@ -0,0 +1,38 @@
+// /api/src/index.ts
+
+import express, { Request, Response, NextFunction } from 'express';
+import promMid from 'express-prometheus-middleware';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to check for Authorization header
+const checkAuthorization = (req: Request, res: Response, next: NextFunction) => {
+  const authToken = req.header('Authorization');
+  if (authToken !== 'mysecrettoken') {
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+  next();
+};
+
+// Middleware to add CORS headers if required
+// app.use(cors());
+
+// Middleware to add Prometheus metrics
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+}));
+
+// Import the controllers
+import { getTime } from './timeController';
+import { getMetrics } from './metricsController';
+
+// Routes
+app.get('/time', checkAuthorization, getTime);
+app.get('/metrics', checkAuthorization, getMetrics);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
